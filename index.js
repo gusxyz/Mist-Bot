7bb73ac6dafbe3c29c9791af48dbaa824c7cd64c
Dawn <august.eymann@gmail.com>
2018-01-04 12:56:11 -0800

    Worked on a better working userinfo command

--- index.js
+++ index.js
@@ -94,20 +94,24 @@ if (message.content.startsWith(Prefix + "giverole")) {
         console.log(`${args[2]} ${args[3]}`)
 } else
 
-  if (message.content.startsWith(Prefix + "info")) {
+  if (message.content.startsWith(Prefix + "userinfo")) {
+    var mtarget = message.guild.member(message.mentions.users.first())
     var target = message.mentions.users.first()
+    if(target) {
           var embed = new Discord.RichEmbed()
         .setAuthor(target.username, target.avatarURL)
         .setColor("RANDOM")
         .setThumbnail(target.avatarURL)
         .addField("Tag", target.tag, true)
         .addField("ID", target.id, true)
-        .addField("Roles", )
+        .addField("Highest role", mtarget.highestRole, true)
         .addField("Created", target.createdAt, true)
         .addField("Status", target.presence.status, true)
-        .addField("Game", target.presence.game, true)
         .setFooter(client.user.username, client.user.avatarURL)
         message.channel.sendEmbed(embed)
+      } else {
+        message.reply("Cant get that user try again.")
+      }
 
   } else
  if (message.content.startsWith(Prefix + "lmgtfy")) {
