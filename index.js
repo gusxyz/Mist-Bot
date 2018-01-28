e7b3506d2408c7e9e7ebb0764c97161cd944bebd
Dawn <august.eymann@gmail.com>
2018-01-27 22:06:42 -0800

    Quick Update

--- index.js
+++ index.js
@@ -97,6 +97,29 @@ client.on("message", function (message) {
         message.react("❌")
       })
   } else
+  if (message.content.toLowerCase().startsWith(Prefix + "nytimes")) {
+    request({
+      url: "https://api.nytimes.com/svc/search/v2/articlesearch.json",
+      qs: {
+        'api-key': "103bcbbc35c6467791de7991b6a90e6b",
+        'q': argz
+      },
+    }, function (error, response, body) {
+    var body = JSON.parse(body)
+    var articles = body["response"].docs
+    var number = Math.floor(Math.random() * articles.length)
+    var article = articles[number]
+    var embed = new Discord.RichEmbed()
+    .setAuthor("The New York Times","http://images.genius.com/b2a54275b0619d84fe7a1abf5fb4d9cb.1000x1000x1.png")
+    .setColor("RANDOM")
+    .setTitle(article["headline"]["main"])
+    .setURL(article["web_url"])
+    .setDescription(article["snippet"])
+    embed.setThumbnail(`https://nytimes.com/${article.multimedia[0]["url"]}`)
+    embed.setFooter(body["copyright"])
+    message.channel.sendEmbed(embed)
+    });
+  }else
   if (message.content.toLowerCase().startsWith(Prefix + "steal")) {
     message.channel.send("YOU WANNA STEAL MY CLOUT? :b: :regional_indicator_e: :regional_indicator_g: :regional_indicator_o: :regional_indicator_n: :regional_indicator_e:  :regional_indicator_t: :regional_indicator_h: :regional_indicator_o: :regional_indicator_t:")
   } else
