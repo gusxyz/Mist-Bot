b1d1e6dbdd26391cd456b6e90d53f922f992e392
August Eymann <august.eymann@gmail.com>
2019-07-11 09:35:15 -0700

    Fixed NSFW Command

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,13 +1,34 @@
 const discord = require('discord.js');
-const unirest = require('unirest');
-
+const request = require('request');
 module.exports.run = async (bot, message, args) => {
-    unirest.get("https://steppschuh-json-porn-v1.p.rapidapi.com/image/5817567562170368/400.jpg")
-        .header("X-RapidAPI-Host", "steppschuh-json-porn-v1.p.rapidapi.com")
-        .header("X-RapidAPI-Key", "oQwqWMGzNVmshDQrH7R3BY8j4DRJp1ubjhUjsnMIjQOrRc0qbC")
-        .end(function (result) {
-        console.log(result.body);
-    });
+   
+  request('https://reddit.com/r/gonewild/random.json', function (error, response, body) {
+    body = JSON.parse(body)
+    var bod = body[0]
+    var data = bod['data']
+    var children = data['children']
+    var childre = children[0]
+    var data2 = childre['data']
+    if(data2['url'].match('.jpg') || data2['url'].match('.png')) {
+    var embed = new discord.RichEmbed()
+    .setColor('RANDOM')
+    .setAuthor('Lockebot')
+    .setTitle('r/gonewild')
+    .setURL(`https://reddit.com${data2['permalink']}`)
+    .setFooter(`Meme by ${data2['author']}`)
+    .setImage(data2['url'])
+    message.channel.send({embed});
+    } else {
+     var embed = new discord.RichEmbed()
+     .setColor('RANDOM')
+     .setAuthor('Lockebot')
+     .setTitle('r/gonewild')  
+     .setURL(`https://reddit.com${data2['permalink']}`)
+     .setFooter(`Image by ${data2['author']}`)
+     .setImage(data2['url']+'.jpg')
+     message.channel.send(embed);
+    }
+ })
 }
 
 module.exports.help = {
